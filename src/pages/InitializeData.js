7d5d0e29e06ab375d5a46b565ffd0a320283f812
letrananhvu <letrananhvu>
2020-07-27 21:42:21 +0300

    seed done

--- src/pages/InitializeData.js
+++ src/pages/InitializeData.js
@@ -3,27 +3,36 @@ import {Container} from 'reactstrap'
 import LoadingBar from '../components/common/LoadingBar'
 import seedApi from '../apis/SeedApi'
 import history from "../helpers/history";
+
 const InitializeData = () => {
     const [status1, setStatus1] = useState('')
     const [status2, setStatus2] = useState('')
     const [status3, setStatus3] = useState('')
+    const [available, setAvailable] = useState(false)
+    const [error, setError] = useState('')
     useEffect(() => {
         seedApi.user().then(() => {
             setStatus1('ok')
+        }).catch(() => {
+            setError('There is some error. Refresh the page!')
         })
         seedApi.restaurants().then(() => {
             setStatus2('ok')
+        }).catch(() => {
+            setError('There is some error. Refresh the page!')
         })
         seedApi.restaurantImages().then(() => {
             setStatus3('ok')
+        }).catch(() => {
+            setError('There is some error. Refresh the page!')
         })
-    },[]);
+    }, []);
 
     useEffect(() => {
-        if(status1 && status2 && status3) {
-            history.push('/')
+        if (status1 && status2 && status3) {
+            setAvailable(true)
         }
-    },[status1, status2, status3])
+    }, [status1, status2, status3])
 
     return (
         <Container>
@@ -32,8 +41,17 @@ const InitializeData = () => {
             {status1 && <p>Seed user: done</p>}
             {status2 && <p>Seed restaurants: done</p>}
             {status3 && <p>Seed image for restaurants: done</p>}
-            <p>* seed image may take several minutes. pls wait </p>
-            <LoadingBar time='1s'/>
+
+
+            <p>{error}</p>
+            {
+                available ? <button onClick={() => history.push('/')}>Data is ready!</button>
+                    :
+                    <>
+                        <p>* seed image may take several minutes. pls wait </p>
+                        <LoadingBar time='1s'/>
+                    </>
+            }
         </Container>
     )
 };
